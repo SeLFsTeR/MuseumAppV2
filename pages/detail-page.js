7930f9129fbd817d669a6e266e2faef192a4b26b
SeLFsTeR <p.a.meinema@gmail.com>
2019-10-11 15:33:41 +0200

    Alles tot nu toe

---
+++ pages/detail-page.js
@@ -0,0 +1,21 @@
+// add event handler
+function submitComment() {
+    // gather data
+    const inputField = document.getElementById('name')
+    const name = inputField.value
+    const textArea = document.getElementById('msg')
+    const msg = textArea.value
+    
+    // create the elements you need
+    const comment = document.createElement('section')
+    const h3 = document.createElement('h3')
+    const p = document.createElement('p')
+
+    h3.innerHTML = `${name} said:` // <------ dus dit, goed lezen wat er staat!
+    p.innerHTML = `${msg}`
+    comment.classList.add('comment')
+    comment.appendChild(h3)
+    comment.appendChild(p)
+
+    
+  }
